0f5d46696aa5656618b3784f8b52bb3a286d1b8d
Felca <61934585+Felca@users.noreply.github.com>
2024-06-13 13:05:31 +0700

    Update script2.js

--- 1.LandingPage/js/script2.js
+++ 1.LandingPage/js/script2.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(() => {
-        window.location.href = "1.LandingPage/loading3.html";
+        window.location.href = "./loading3.html";
     }, 1500);
 });
