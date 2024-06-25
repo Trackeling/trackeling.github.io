ab1cd031cffbd22bd3750880d9693349b115c7ab
lisanstsya <145576145+lisanstsya@users.noreply.github.com>
2024-06-25 17:29:49 +0700

    Update customerRegis.js

--- 4.CustomerSide/js/customerRegis.js
+++ 4.CustomerSide/js/customerRegis.js
@@ -1,109 +1,111 @@
-const nama = document.getElementById('Name');
-const address = document.getElementById('Address');
-const email = document.getElementById('Email');
-const passW = document.getElementById('PassW');
-const TermsAndCond = document.getElementById('SdanKCheck');
+document.getElementById('buttonSubmit').addEventListener('click', function(event) {
+    event.preventDefault(); 
+    checkValidation();
+});
 
-var danger = '';
+    const nama = document.getElementById('Name');
+    const address = document.getElementById('Address');
+    const email = document.getElementById('Email');
+    const passW = document.getElementById('PassW');
+    const TermsAndCond = document.getElementById('SdanKCheck');
+
+    var danger = '';
 
 function checkValidation(){
     const isNameValid = checkName(nama);
     const isAddressValid = checkAddress(address);
-    const isEmailValid = checkEmail(email)
+    const isEmailValid = checkEmail(email);
     const isPassValid = checkPass(passW);
-    const isSdanKAggree = checkSNK(TermsAndCond);
+    const isSdanKAgree = checkSNK(TermsAndCond);
 
-    if(isNameValid && isAddressValid && isEmailValid && isPassValid && isSdanKAggree){
+    if (isNameValid && isAddressValid && isEmailValid && isPassValid && isSdanKAgree) {
         location.href = "customerHome.html";
-    }
-    else{
+    } else {
         alert(danger);
     }
-    danger = '';
-}
+    danger = ''; 
+};
 
-function checkName(input){
+function checkName(input) {
     const inputValue = input.value.trim();
     const regx = /^[a-zA-Z\s]+$/;
-    
-    if(inputValue === ''){
-        danger += "Name shouldn't be empty!\n";
+
+    if (inputValue === '') {
+        danger += "Nama tidak boleh kosong!\n";
         return false;
     }
 
-    if(inputValue.length <= 3){
-        danger += "Name should have more than 3 characters\n";
+    if (inputValue.length <= 3) {
+        danger += "Nama harus memiliki lebih dari 3 karakter!\n";
         return false;
     }
 
-    if(!regx.test(inputValue)){
-        danger += "Name should only contain alphabet!\n";
+    if (!regx.test(inputValue)) {
+        danger += "Nama hanya boleh mengandung huruf!\n";
         return false;
     }
 
     return true;
 }
 
-function checkAddress(input){
+function checkAddress(input) {
     const inputValue = input.value.trim();
     const regx = /^[a-zA-Z0-9\.,\s]+$/;
 
-    if(inputValue === ''){
-        danger += "Address shouldn't be empty!\n";
+    if (inputValue === '') {
+        danger += "Alamat tidak boleh kosong!\n";
         return false;
     }
 
-    if(inputValue.length <= 10){
-        danger += "Address should have more than 10 characters\n";
+    if (inputValue.length <= 10) {
+        danger += "Alamat harus memiliki lebih dari 10 karakter!\n";
         return false;
     }
 
-    if(!regx.test(inputValue)){
-        danger += "Wrong address format!\n";
+    if (!regx.test(inputValue)) {
+        danger += "Format alamat salah!\n";
         return false;
     }
-    
+
     return true;
 }
 
-function checkEmail(input){
+function checkEmail(input) {
     const inputValue = input.value.trim();
     const regx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-    console.log(typeof input);
-
-    if(inputValue === ''){
-        danger += "Email shouldn't be empty!\n";
+    if (inputValue === '') {
+        danger += "Email tidak boleh kosong!\n";
         return false;
     }
 
-    if(!regx.test(inputValue)){
-        danger += "Wrong email format!\n";
+    if (!regx.test(inputValue)) {
+        danger += "Format email salah!\n";
         return false;
     }
-    
+
     return true;
 }
 
-function checkPass(input){
+function checkPass(input) {
     const inputValue = input.value.trim();
-    if(inputValue === ''){
-        danger += "Password shouldn't be empty!\n";
+    if (inputValue === '') {
+        danger += "Password tidak boleh kosong!\n";
         return false;
     }
-    if(inputValue.length < 8){
-        danger += "Password should be more than 8 characters!\n";
+    if (inputValue.length < 8) {
+        danger += "Password harus memiliki lebih dari 8 karakter!\n";
         return false;
     }
-    
+
     return true;
 }
 
-function checkSNK(input){
-    if(input.checked == false){
-        danger += "You must agree to the terms and agreement!\n";
+function checkSNK(input) {
+    if (!input.checked) {
+        danger += "Anda harus menyetujui Syarat dan Ketentuan!\n";
         return false;
     }
-    
+
     return true;
 }
