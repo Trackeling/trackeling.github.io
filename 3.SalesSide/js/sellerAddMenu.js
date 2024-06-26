8306dada80cc556049112fa7bb241a941a3e2302
lisanstsya <145576145+lisanstsya@users.noreply.github.com>
2024-06-26 23:53:14 +0700

    Update sellerAddMenu.js

--- 3.SalesSide/js/sellerAddMenu.js
+++ 3.SalesSide/js/sellerAddMenu.js
@@ -11,58 +11,57 @@ const price = document.getElementById('price');
 var danger = '';
 
 function previewProfilePhotoGerobak(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-  
-    reader.onload = function(e) {
-      document.getElementById("ImageGerobak").src = e.target.result;
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            document.getElementById("ImageGerobak").src = e.target.result;
+        }
+
+        reader.readAsDataURL(input.files[0]);
     }
-  
-    reader.readAsDataURL(input.files[0]);
-  }
 }
 
 function previewProfilePhotoPenjual(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-  
-    reader.onload = function(e) {
-      document.getElementById("MukaPedagang").src = e.target.result;
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            document.getElementById("MukaPedagang").src = e.target.result;
+        }
+
+        reader.readAsDataURL(input.files[0]);
     }
-  
-    reader.readAsDataURL(input.files[0]);
-  }
 }
 
 function previewFotoMenu(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-  
-    reader.onload = function(e) {
-      document.getElementById("FotoMenu").src = e.target.result;
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            document.getElementById("FotoMenu").src = e.target.result;
+        }
+
+        reader.readAsDataURL(input.files[0]);
     }
-  
-    reader.readAsDataURL(input.files[0]);
-  }
 }
 
 function editDeskripsiDagangan() {
-  var pElement = document.querySelector('.deskripsiDagang');
-  var inputElement = document.getElementById('inputDeskripsiDagang');
-  inputElement.value = pElement.innerText;
-  pElement.classList.add('hidden');
-  inputElement.classList.remove('hidden');
-  inputElement.focus();
+    var pElement = document.querySelector('.deskripsiDagang');
+    var inputElement = document.getElementById('inputDeskripsiDagang');
+    inputElement.value = pElement.innerText;
+    pElement.classList.add('hidden');
+    inputElement.classList.remove('hidden');
+    inputElement.focus();
 }
 
 function saveDeskripsiDagangan() {
-  var pElement = document.querySelector('.deskripsiDagang');
-  var inputElement = document.getElementById('inputDeskripsiDagang');
-  pElement.innerText = inputElement.value;
-  inputElement.classList.add('hidden');
-  pElement.classList.remove('hidden');
+    var pElement = document.querySelector('.deskripsiDagang');
+    var inputElement = document.getElementById('inputDeskripsiDagang');
+    pElement.innerText = inputElement.value;
+    inputElement.classList.add('hidden');
+    pElement.classList.remove('hidden');
 }
-
 
 document.getElementById("simpanlanjut").addEventListener("click", function () {
     var menu = document.getElementById("namamenu").value;
@@ -71,6 +70,7 @@ document.getElementById("simpanlanjut").addEventListener("click", function () {
     var category = document.getElementById("selkat").value;
     var backgroundPhoto = document.getElementById("ImageGerobak").src;
     var profilePhoto = document.getElementById("MukaPedagang").src;
+    var menuPhoto = document.getElementById("FotoMenu").src;
     var description = document.getElementById("inputDeskripsiDagang").value;
 
     var menuTrim = menu.trim();
@@ -90,13 +90,18 @@ document.getElementById("simpanlanjut").addEventListener("click", function () {
         return;
     }
 
-    if (backgroundPhoto === '' || backgroundPhoto === 'default-image-path') { // Replace 'default-image-path' with the default src path of the image
+    if (backgroundPhoto === '' || backgroundPhoto === 'assets/icons/iconGerobak.png') { 
         alert("Foto background harus diisi.");
         return;
     }
 
-    if (profilePhoto === '' || profilePhoto === 'default-image-path') { // Replace 'default-image-path' with the default src path of the image
+    if (profilePhoto === '' || profilePhoto === 'assets/images/DefaultFoto.png') { 
         alert("Foto profil harus diisi.");
+        return;
+    }
+
+    if (menuPhoto === '' || menuPhoto === 'assets/icons/BackgroundFotomenu.png') { 
+        alert("Foto menu harus diisi.");
         return;
     }
 
@@ -111,4 +116,5 @@ document.getElementById("simpanlanjut").addEventListener("click", function () {
 
     window.location.href = "homepage(pedagang).html";
 });
+
 
